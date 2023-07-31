a655f60b107c602906d01129e38589300866e939
Carolina Li <67572378+CarolinaLiPage@users.noreply.github.com>
2023-07-30 21:16:42 -0700

    Add files via upload
    Comments were added.
    A review of existing code.
    
    No new code was added.

--- TodoList.js
+++ TodoList.js
@@ -1,3 +1,5 @@
+// 'useState' is a hook used to managing state of components.
+// Use React as the main library to build components.
 import React, {useState} from 'react';
 import {
   View,
@@ -7,25 +9,39 @@ import {
   TouchableOpacity,
 } from 'react-native';
 
+// Define React component. A functional component.
 const TodoList = () => {
+  //  Set up state: the task state holds the the val of the input field,
+  //  where the user can type in the new task to be added.
+  //  The setTask function is used to update the 'task' state.
+  //  The 'task' state is an array that holds the list of task.
+  // Then the setTasks function updates the 'task' array.
   const [task, setTask] = useState('');
   const [tasks, setTasks] = useState([]);
 
+  // Add a task.
   const handleAddTask = () => {
     if (task.trim() === '') {
-      return;
+      // Check if 'task' is an empty string.
+      return; // returns do nothing if it is an empty string.
     }
 
+    // If 'task' is not an empty string.
+    // Use setTasks function to add the new'task' into the task array.
     setTasks(prevTasks => [...prevTasks, task]);
+    // Resets the state of the 'task' to an empty string after adding the task to clear the input field.
     setTask('');
   };
 
+  // Delete a task from the list, takes an 'index' as a parameter.
+  // Indicates the index of the task to be deleted.
   const handleDeleteTask = index => {
-    const updatedTasks = [...tasks];
-    updatedTasks.splice(index, 1);
-    setTasks(updatedTasks);
+    const updatedTasks = [...tasks]; // Create a copy of the 'tasks' array using the spread operator(avoid directly modifying the state)
+    updatedTasks.splice(index, 1); // Use splice to remove the task at the given index.
+    setTasks(updatedTasks); // Update the state of the task.
   };
 
+  // Render the component. Input field: TextInput
   return (
     <View style={styles.container}>
       <Text style={styles.title}>To-Do List</Text>
@@ -53,6 +69,7 @@ const TodoList = () => {
   );
 };
 
+// Style the component. The styles object use 'StyleSheet.create()' to define CSS-like styles for components.
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -104,4 +121,5 @@ const styles = StyleSheet.create({
   },
 });
 
+// Exporting the component.
 export default TodoList;
