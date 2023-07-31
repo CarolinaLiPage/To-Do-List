572297c6578b9a64f23194e44f4cb352dc5476dd
Carolina Li <67572378+CarolinaLiPage@users.noreply.github.com>
2023-07-30 23:02:10 -0700

    Add files via upload
    Display a sign-in screen.

---
+++ SignInScreen.js
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+//import logo from '../../assets/images/do.png';
+
+const SignInScreen = () => {
+  return (
+    <View>
+      <Text>sign In Screen</Text>
+    </View>
+  );
+};
+
+export default SignInScreen;
---
+++ index.js
@@ -0,0 +1,16 @@
+// This file contains helper function that will come in handy in signIn.js
+
+// import SignInScreen from './TodoList/signInScreen';
+import React from 'react';
+import {View, Text} from 'react-native';
+import SignInScreen from '../todo/SignInScreen';
+
+const SignIn = () => {
+  return (
+    <View>
+      <SignInScreen />
+    </View>
+  );
+};
+
+export default SignIn;
